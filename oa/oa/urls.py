b3e797c5267b1118d2bea2e1f7c750dcb9ea4c10
superkevingit <superkevingit@gmail.com>
2016-05-11 14:15:37 +0800

    add undo

--- oa/oa/urls.py
+++ oa/oa/urls.py
@@ -1,14 +1,14 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
-from message.views import OaMessageListView, OaMessageDetailView, new_oa_message, del_oa_message
+from message.views import OaMessageListView, OaMessageDetailView, new_oa_message, del_oa_message, undo_del_oa_message
 
 urlpatterns = [
     url(r'^message/$', OaMessageListView.as_view(), name='oa-message-list'),
     url(r'^message/(?P<pk>\d+)/$', OaMessageDetailView.as_view(), name='oa-message-datail'),
     url(r'^message/new/$', new_oa_message, name='oa-message-new'),
     url(r'^message/del/(\d+)/$', del_oa_message, name='oa-message-del'),
-#    url(r'^message/undo_del/(?P<pk>\d+)$', undo_del_oa_message, name='oa-message-del-undo'),
+    url(r'^message/undo_del/(\d+)$', undo_del_oa_message, name='oa-message-del-undo'),
 
     url(r'^admin/', include(admin.site.urls)),
 ]
